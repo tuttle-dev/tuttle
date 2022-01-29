68f342a3e73ee3af6dc7080f62769fb8c0fd3923
Christian Staudt <mail@clstaudt.me>
2022-01-29 20:29:53 +0000

    bumped version to 0.0.3

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/tuttle-dev/tuttle",
-    version="0.0.2",
+    version="0.0.3",
     zip_safe=False,
 )
