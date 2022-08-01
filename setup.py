0165aed0ce3cd4942bf1bbdcfe8c5b74dabac075
Christian Staudt <mail@clstaudt.me>
2022-08-01 13:06:05 +0200

    version 0.0.5

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     packages=find_packages(include=["tuttle", "tuttle.*"]),
     test_suite="tests",
     url="https://github.com/tuttle-dev/tuttle",
-    version="0.0.4",
+    version="0.0.5",
     zip_safe=False,
 )
--- tuttle/__init__.py
+++ tuttle/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for tuttle."""
 
 __author__ = """Christian Staudt"""
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 from . import (
     app,
