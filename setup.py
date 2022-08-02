6f8316d94901f3ca023258031430532aa929c340
Christian Staudt <mail@clstaudt.me>
2022-08-02 23:01:14 +0200

    patch version up

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     packages=find_packages(include=["tuttle", "tuttle.*"]),
     test_suite="tests",
     url="https://github.com/tuttle-dev/tuttle",
-    version="0.0.8",
+    version="0.0.9",
     zip_safe=False,
 )
--- tuttle/__init__.py
+++ tuttle/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for tuttle."""
 
 __author__ = """Christian Staudt"""
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 
 from . import (
     app,
