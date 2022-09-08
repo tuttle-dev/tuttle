6ca10d51545628a199eb3e368c111dc40dd55086
Christian Staudt <mail@clstaudt.me>
2022-09-08 15:02:18 +0200

    removed requirement fints to make pyinstaller work

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ from flet import (
     VerticalDivider,
     Icon,
 )
-from flet import icons
+from flet import icons, colors
 
 from tuttle.controller import Controller
 
@@ -92,26 +92,60 @@ def main(page: Page):
         con,
     )
 
-    # navigation = NavigationRail(
-    #     selected_index=0,
-    #     label_type="all",
-    #     extended=True,
-    #     min_width=100,
-    #     min_extended_width=400,
-    #     # leading=FloatingActionButton(icon=icons.CREATE, text="Add"),
-    #     group_alignment=-0.9,
-    #     destinations=[
-    #         NavigationRailDestination(
-    #             icon=icons.AREA_CHART_OUTLINED,
-    #             selected_icon=icons.AREA_CHART,
-    #             label="Dashboard",
-    #         ),
-    #     ],
-    #     on_change=lambda e: print("Selected destination:", e.control.selected_index),
-    # )
+    rail = NavigationRail(
+        selected_index=0,
+        label_type="all",
+        extended=True,
+        min_width=100,
+        min_extended_width=250,
+        group_alignment=-0.9,
+        bgcolor=colors.BLUE_GREY_900,
+        destinations=[
+            NavigationRailDestination(
+                icon=icons.SPEED,
+                label="Dashboard",
+            ),
+            NavigationRailDestination(
+                icon=icons.WORK,
+                label="Projects",
+            ),
+            NavigationRailDestination(
+                icon=icons.DATE_RANGE,
+                label="Time Tracking",
+            ),
+            NavigationRailDestination(
+                icon=icons.CONTACT_MAIL,
+                label="Contacts",
+            ),
+            NavigationRailDestination(
+                icon=icons.HANDSHAKE,
+                label="Clients",
+            ),
+            NavigationRailDestination(
+                icon=icons.HISTORY_EDU,
+                label="Contracts",
+            ),
+            NavigationRailDestination(
+                icon=icons.OUTGOING_MAIL,
+                label="Invoices",
+            ),
+            NavigationRailDestination(
+                icon=icons.SETTINGS,
+                label_content=Text("Settings"),
+            ),
+        ],
+        on_change=lambda e: print("Selected destination:", e.control.selected_index),
+    )
 
     page.add(
-        app,
+        Row(
+            [
+                rail,
+                # VerticalDivider(width=1),
+                Column([Text("Body!")], alignment="start", expand=True),
+            ],
+            expand=True,
+        )
     )
 
     page.update()
--- tuttle/banking.py
+++ tuttle/banking.py
@@ -1,31 +1,32 @@
 """Online banking functionality."""
 
 import getpass
-from fints.client import FinTS3PinTanClient
+
+# from fints.client import FinTS3PinTanClient
 from loguru import logger
 
 from .model import BankAccount, Bank
 
 
-class Banking:
-    """."""
+# class Banking:
+#     """."""
 
-    def __init__(self, bank: Bank):
-        self.bank = bank
-        self.product_id = None  # TODO: register product ID before deployment
+#     def __init__(self, bank: Bank):
+#         self.bank = bank
+#         self.product_id = None  # TODO: register product ID before deployment
 
-    def connect(self):
-        """Connect to the online banking interface via FinTS."""
-        self.client = FinTS3PinTanClient(
-            self.bank.BLZ,  # Your bank's BLZ
-            getpass.getpass("user name: "),  # Your login name
-            getpass.getpass("PIN:"),  # Your banking PIN
-            "https://hbci-pintan.gad.de/cgi-bin/hbciservlet",
-            product_id=self.product_id,
-        )
+#     def connect(self):
+#         """Connect to the online banking interface via FinTS."""
+#         self.client = FinTS3PinTanClient(
+#             self.bank.BLZ,  # Your bank's BLZ
+#             getpass.getpass("user name: "),  # Your login name
+#             getpass.getpass("PIN:"),  # Your banking PIN
+#             "https://hbci-pintan.gad.de/cgi-bin/hbciservlet",
+#             product_id=self.product_id,
+#         )
 
-    def get_accounts(self):
-        """List SEPA accounts."""
-        with self.client as client:
-            accounts = client.get_sepa_accounts()
-            return accounts
+#     def get_accounts(self):
+#         """List SEPA accounts."""
+#         with self.client as client:
+#             accounts = client.get_sepa_accounts()
+#             return accounts
