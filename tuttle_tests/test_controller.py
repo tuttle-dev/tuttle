1137bf6b721cab89fb2e4ad36803d7294c58039b
Christian Staudt <mail@clstaudt.me>
2022-09-12 08:34:17 +0200

    ✅ unit tests

--- tuttle_tests/test_controller.py
+++ tuttle_tests/test_controller.py
@@ -4,7 +4,7 @@
 
 import pytest
 
-from tuttle import controller
+from tuttle import controller, preferences
 
 
 def test_eval_time_planning():
@@ -14,5 +14,5 @@ def test_eval_time_planning():
 
 def test_instantiate_controller():
     """Test that the controller can be instantiated."""
-    con = controller.Controller(in_memory=True)
+    con = controller.Controller(in_memory=True, preferences=preferences.Preferences())
     assert con is not None
