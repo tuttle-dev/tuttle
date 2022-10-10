7ec232f361da3ff1cd90a373474cc0abd44f8c7f
Christian Staudt <mail@clstaudt.me>
2022-10-10 15:30:27 +0200

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     packages=find_packages(include=["tuttle", "tuttle.*", "tuttle_tests"]),
     test_suite="tests",
     url="https://github.com/tuttle-dev/tuttle",
-    version="0.1.0",
+    version="0.2.0",
     zip_safe=False,
 )
