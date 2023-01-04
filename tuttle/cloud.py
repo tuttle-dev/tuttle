393732c979af7b757a33e041844d08fd4d856b4a
Christian Staudt <mail@clstaudt.me>
2023-01-04 15:54:02 +0100

    replace pyicloud with icloudpy

--- tuttle/cloud.py
+++ tuttle/cloud.py
@@ -1,7 +1,7 @@
 """Cloud connectors."""
 
 import getpass
-import pyicloud
+import icloudpy
 
 from .model import ICloudAccount, GoogleAccount
 
@@ -9,7 +9,7 @@ from .model import ICloudAccount, GoogleAccount
 def login_iCloud(user_name: str):
     """Log in to Apple iCloud"""
     password = getpass.getpass(prompt=f"iCloud password for account {user_name}: ")
-    iCloud = pyicloud.PyiCloudService(user_name, password)
+    iCloud = icloudpy.ICloudPyService(user_name, password)
     if not iCloud.is_trusted_session:
         result = iCloud.validate_2fa_code(getpass.getpass(prompt="verification code: "))
     assert iCloud.is_trusted_session
