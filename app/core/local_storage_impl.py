79d192de7fa031b1397ceb4f463a71db58794ca2
Vladimir Peter <vlad.edna@gmail.com>
2023-01-10 01:35:00 +0800

    feat: implemented local storage

--- app/core/local_storage_impl.py
+++ app/core/local_storage_impl.py
@@ -1,27 +1,33 @@
 from core.abstractions import ClientStorage
 from flet import Page
+import time, threading
 
-# TODO - remove the cache with actual local storage
+
 class ClientStorageImpl(ClientStorage):
     """Flet's client storage API allows storing key-value data on a client side in a persistent storage"""
 
     def __init__(self, page: Page):
         super().__init__()
         self.page = page
-        self.cache = {}
 
     def set_value(self, key: str, value: any):
         prefixedKey = self.keys_prefix + key
-        self.cache[prefixedKey] = value
+        self.th = threading.Thread(
+            target=lambda: self.page.client_storage.set(prefixedKey, value),
+            args=(),
+            daemon=True,
+        )
+        self.th.start()
 
     def get_value(self, key: str):
         prefixedKey = self.keys_prefix + key
-        keyExists = prefixedKey in self.cache.keys()
+        keyExists = self.page.client_storage.contains_key(prefixedKey)
         if not keyExists:
             return None
-        return self.cache[prefixedKey]
+        return self.page.client_storage.get(prefixedKey)
 
     def remove_value(self, key: str):
         prefixedKey = self.keys_prefix + key
-        if prefixedKey in self.cache:
-            del self.cache[prefixedKey]
+        keyExists = self.page.client_storage.contains_key(prefixedKey)
+        if keyExists:
+            self.page.client_storage.remove(prefixedKey)
