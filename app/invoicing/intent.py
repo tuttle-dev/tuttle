226da236a9a65b969c7f77611ab780896a9fb42c
Christian Staudt <mail@clstaudt.me>
2023-01-11 15:37:39 +0100

    WIP: InvoicingIntent

--- app/invoicing/intent.py
+++ app/invoicing/intent.py
@@ -1,9 +1,18 @@
 """  """
 
+from core.abstractions import IntentResult
 
 from .data_source import InvoicingDataSource
+
+from projects.data_source import ProjectDataSource
 
 
 class InvoicingIntent:
     def __init__(self):
-        pass
+        self.project_data_source = ProjectDataSource()
+
+    def get_all_projects_as_map(self) -> IntentResult:
+        """returns a list of active projects"""
+        # FIXME: why a method that does nothing but deletage to another method?
+        result = self.project_data_source.get_all_projects_as_map()
+        return IntentResult(was_intent_successful=True, data=[])
