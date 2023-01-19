a5b50268ed04a48bc274b4ee3cff90a81079ba83
Christian Staudt <mail@clstaudt.me>
2023-01-19 21:42:45 +0100

    WIP: restrict Contact deletion on IntegrityError

---
+++ app/core/exceptions.py
@@ -0,0 +1,19 @@
+from typing import List, Any
+
+
+class DataIntegrityViolation(Exception):
+    """
+    Exception raised when an operation is not permitted because it would interfere with data integrity.
+
+    Args:
+        message (str): Error message to indicate the reason of the exception
+        dependent_objects (List[Any], optional): A list of dependent objects whose integrity would be violated by the operation. Default is None
+    """
+
+    def __init__(
+        self,
+        message: str,
+        dependent_objects: List[Any] = None,
+    ):
+        self.message = message
+        self.dependent_objects = dependent_objects
