4bf6f8d36ba96d16af1084a59208d5d4982196c1
Vladimir Peter <vlad.edna@gmail.com>
2023-01-20 21:22:48 +0800

    fix: removed time tracking un clean implementations

--- app/timetracking/data_source.py
+++ app/timetracking/data_source.py
@@ -5,7 +5,6 @@ from core.intent_result import IntentResult
 from pandas import DataFrame
 
 from tuttle.calendar import ICSCalendar
-from tuttle.cloud import CloudLoginResult, login_iCloud, verify_icloud_session
 from tuttle.dev import singleton
 
 from .model import CloudCalendarInfo, CloudConfigurationResult
@@ -92,104 +91,40 @@ class TimeTrackingCloudCalendarSource:
     def __init__(self):
         super().__init__()
 
-    def load_cloud_calendar_data(
-        self, info: CloudCalendarInfo, cloud_session: any
-    ) -> IntentResult:
-        """TODO Loads data given CloudCalendarInfo
+    def load_cloud_calendar_data(self, calendar_name: str):
+        """TODO Loads data from a cloud calendar"""
+        raise NotImplementedError
 
-        Params:
-            info :
-                information about the calendar - name, account, provider
-            cloud_session :
-                a reference to the authenticated cloud session
-        Returns:
-            IntentResult:
-                was_intent_successful : bool
-                data :  CloudConfigurationResult(
-                        calendar_loaded_successfully = True
-                ) if was_intent_successful else None
-                log_message  : str  if an error or exception occurs
-                exception : Exception if an exception occurs
-        """
-        try:
-            return IntentResult(
-                was_intent_successful=False,
-                log_message="Not implemented",
-            )
-        except Exception as e:
-            return IntentResult(
-                was_intent_successful=False,
-                log_message=f"Exception raised @TimeTrackingDataSource._load_cloud_calendar {e.__class__.__name__}",
-                exception=e,
-            )
-
-    """ ICLOUD LOGIN STEPS """
+    """ iCLOUD LOGIN STEPS """
 
     def login_to_icloud(
         self,
-        calendar_info: CloudCalendarInfo,
+        icloud_account: str,
+        icloud_account_password: str,
     ):
-        """Attempts to authenticate user with their icloud account
-
-        Returns IntentResult
-        -------
-            data : [CloudCalendarInfo] if was_intent_successful else None
-
-        """
-        cloud_login_result: CloudLoginResult = login_iCloud(
-            user_name=calendar_info.account, password=calendar_info.password
-        )
-        return IntentResult(
-            was_intent_successful=True,
-            data=CloudConfigurationResult(
-                request_2fa_code=cloud_login_result.request_2fa_code,
-                session_ref=cloud_login_result.icloud_session_ref,
-                cloud_acc_configured_successfully=cloud_login_result.is_logged_in,
-                auth_error_occurred=cloud_login_result.auth_error_occured,
-            ),
-        )
+        """TODO Attempts to authenticate user with their icloud account"""
+        raise NotImplementedError
 
     def verify_icloud_with_2fa(
         self,
-        login_result: CloudConfigurationResult,
         two_factor_code: str,
     ) -> IntentResult:
-        """Attempts to verify an icloud session given a 2fa code
-
-        Returns
-        -------
-            data as CloudConfigurationResult
-        """
-        session_res = verify_icloud_session(
-            iCloud=login_result.session_ref,
-            two_factor_auth_code=two_factor_code,
-        )
-        return IntentResult(
-            data=CloudConfigurationResult(
-                cloud_acc_configured_successfully=session_res.is_logged_in,
-                session_ref=session_res.icloud_session_ref,
-            )
-        )
+        """TODO Attempts to verify an icloud session given a 2fa code"""
+        raise NotImplementedError
 
     """ GOOGLE LOGIN STEPS """
 
     def login_to_google(
         self,
-        calendar_info: CloudCalendarInfo,
+        google_account: str,
+        google_account_password: str,
     ):
-        # TODO implement
-        return IntentResult(
-            was_intent_successful=False,
-            log_message="Not implemented",
-        )
+        """TODO Attempts to authenticate user with their google account"""
+        raise NotImplementedError
 
     def verify_google_with_2fa(
         self,
-        login_result: CloudConfigurationResult,
         two_factor_code: str,
-    ):
-        # TODO implement
-        return IntentResult(
-            was_intent_successful=False,
-            log_message="Not implemented",
-        )
+    ) -> IntentResult:
+        """TODO Attempts to verify an google session given a 2fa code"""
+        raise NotImplementedError
