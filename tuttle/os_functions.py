112a5c4fbc59ae96cf34d7002b9e8d9407174c90
Christian Staudt <mail@clstaudt.me>
2023-04-24 11:59:25 +0200

    pdf preview: raise exception on process call error

--- tuttle/os_functions.py
+++ tuttle/os_functions.py
@@ -18,16 +18,21 @@ def open_application(app_name):
 
 def preview_pdf(file_path):
     """Preview a PDF file."""
-    if not Path(file_path).exists():
-        raise FileNotFoundError(f"File not found: {file_path}")
-    if platform.system() == "Darwin":
-        os.system("qlmanage -p {}".format(file_path))
-    elif platform.system() == "Windows":
-        os.startfile(file_path)
-    elif platform.system() == "Linux":
-        os.system("xdg-open {}".format(file_path))
-    else:
-        print("Sorry, your platform is not supported.")
+    try:
+        if not Path(file_path).exists():
+            raise FileNotFoundError(f"File not found: {file_path}")
+        if platform.system() == "Darwin":
+            subprocess.check_call(["qlmanage", "-p", file_path])
+        elif platform.system() == "Windows":
+            os.startfile(file_path)
+        elif platform.system() == "Linux":
+            subprocess.check_call(["xdg-open", file_path])
+        else:
+            raise RuntimeError("Sorry, your platform is not supported.")
+    except subprocess.CalledProcessError as err:
+        raise RuntimeError(
+            f"Error occurred while opening the PDF file. Return code: {err.returncode}. Error: {err.output}"
+        )
 
 
 def open_folder(folder_path):
